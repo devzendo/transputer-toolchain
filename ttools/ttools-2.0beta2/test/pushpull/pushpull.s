4aa674e7ef803349f9c822090b651532f267107f
Matt Gumbley <matt.gumbley@devzendo.org>
2018-03-23 08:45:29 +0000

    Fixes in names of assembly files, for case insensitive filesystems.

--- ttools/ttools-2.0beta2/test/pushpull/pushpull.s
+++ ttools/ttools-2.0beta2/test/pushpull/pushpull.s
@@ -1,4 +1,3 @@
-# 1 "pushpull.S"
 //
 // pushpull --
 //   Simple sample program for data exchange via the communication board.
@@ -15,228 +14,273 @@
 // 
 // 940621, sizif
 
-# 1 "pushpull.h" 1
- 
+#include "pushpull.h"
 
+#define LOOPBACK  /* for test: internal loopback via a memory channel */
 
+/* useful macros make life easier */
+#define LEND(ctrl,label)        ldlp ctrl; ldc 9f-label; lend; 9: ;
+#define LDPI(label)             ldc label-9f; ldpi; 9: ;
+#define NEQC(const)             eqc const; eqc 0;
+#define LE                      gt; eqc 0;
+#define GE                      rev; LE;
+#define LT                      rev; gt;
+#define BR                      eqc 0; cj   /* traditional "branch if true" */
+#define LDUP(local_index)       stl local_index; ldl local_index;
+#define LDS(label)              LDPI(label); ldnl 0;         /* load static */
+#define STS(label)              LDPI(label); stnl 0;        /* store static */
+#define LDSB(label)             LDPI(label); ld;
+#define STSB(label)             LDPI(label); sb;
 
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-# 17 "pushpull.S" 2
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-  
-  
-  
-  
-
+/* which links we should use */
+#ifndef LOOPBACK
+  #define PUSH_LINK  (0x80000000)  /* Link 0 Output */
+  #define PULL_LINK  (0x80000014)  /* Link 1 Input */
+  #define LOAD_PUSH_LINK	mint;
+  #define LOAD_PULL_LINK	mint; ldnlp +5;
+#else /* LOOPBACK */
+  #define PUSH_LINK  (0x80000080)
+  #define PULL_LINK  PUSH_LINK
+  #define LOAD_PUSH_LINK	mint; ldnlp +0x20
+  #define LOAD_PULL_LINK	LOAD_PUSH_LINK
+#endif /* LOOPBACK */
 
 .globl head@@start;
  head@@start = 0x80008000;
 
-	 
-
-
+	/*
+         * Startup code
+         */
 
 .globl @@hook
 @@hook:
-	 
-	ldc 0; ldc  (0x80009000-0x20+0x00)  -9f; ldpi; 9: ; ; stnl +0;
-	ldc 0; ldc  (0x80009000-0x20+0x04)  -9f; ldpi; 9: ; ; stnl +0;
-	ldc 0; ldc  (0x80009000-0x20+0x08)  -9f; ldpi; 9: ; ; stnl +0;
-	ldc 0; ldc  (0x80009000-0x20+0x0c)  -9f; ldpi; 9: ; ; stnl +0;
+	/* Initialize status words for all four buffers */
+	ldc 0; LDPI (PUSH_STAT1); stnl +0;
+	ldc 0; LDPI (PUSH_STAT2); stnl +0;
+	ldc 0; LDPI (PULL_STAT1); stnl +0;
+	ldc 0; LDPI (PULL_STAT2); stnl +0;
 
-
-	 
+#ifdef LOOPBACK
+	/* Initialize the memory link used for loopback */
 	mint;
-	ldc   (0x80000080)   -9f; ldpi; 9: ; ; stnl 0; 
+	STS (PUSH_LINK)
+#endif /* LOOPBACK */
+
+/* For testing on-board memory with the transputer passive */
+#if 0
+/* when we do just stopp here, everything goes fine, even with /usr/bin/emacs */
+	stopp
+#elif 0
+/* try to do something irrelevant with memory R/W: may it will interfere?
+   nope; everything's fine */
+  1:
+	ldc 0xaa55aa55;
+	ldc 0x80005000;
+	stnl +7
+	j 1b
+#elif 0
+/* try to do something like what we do under normal operation: block move */
+  1:
+//	LDPI (PUSH_BUF1)
+//	LDPI (0x80007000)
+	LDPI (0x80006000)
+	LDPI (0x80006000)
+//	ldc PUSHPULL_BUFSIZE
+//	ldc 2048+1024 		// No errors
+//	ldc 2048+1024+512	// No errors
+//	ldc 2048+1024+512+256	// Very rare errors
+	ldc 2048+1024+1024	// Errors 50/50
+	move
+	j 1b
+#elif 0
+/* see if moving memory block by pieces interfere as well...
+   ... it does.  */
+  1:
+	LDPI (0x80007000)	// from
+	stl 0
+	LDPI (0x80006000)	// to
+	stl 1
+	ldc 4096		// count
+	stl 2
+  2:	
+	ldl 0; ldnl 0
+	ldl 1; stnl 0
+	ldl 0; ldnlp 1; stl 0	
+	ldl 1; ldnlp 1; stl 1
+	ldl 2; adc -4; stl 2
+	ldc 1; ldl 2; gt; cj 2b
+	j 1b
+#elif 0
+/* so it seems that any write operation from transputer is dangerous.
+   Try to simplify the test case */
+	// Move the program to the internal memory (which is safe from the corruption)
+	LDPI (1f)
+	mint; adc 0xf0	// plcace the program at 0x800000f0
+	ldc 2f-1f
+	move
+	// Put destination address to the workspace
+	LDPI (0x80006000 + 4095)
+	stl 1
+//	mint; adc 0xf0; gcall
+  1:
+	ldl 1; stnl 0	// write a random garbage to the address specified
+	ldl 1; stnl 0	// write a random garbage to the address specified
+	ldl 1; stnl 0	// write a random garbage to the address specified
+	ldl 1; stnl 0	// write a random garbage to the address specified
+	ldl 1; stnl 0	// write a random garbage to the address specified
+	j 1b
+  2:
+#elif 0
+  1:
+	LDPI (0x80007000+2048+1024+512)
+	LDPI (0x80006000+2048+1024+512)
+	ldc 512
+	move
+	j 1b
+#endif
 
 
- 
-# 140 "pushpull.S"
-
-
-
-	 
+	/* start transmitter process */
 	ldc push - 9f
 	ldlp +0x50
 	startp;
   9:
-	 
+	/* start receiver process */
 	ldc pull - 9f
 	ldlp +0xa0
 	startp;
   9:
-	 
+	/* ... and die. */
 	stopp
 
 
-	 
-
-
+	/*
+         * Transmitter process code
+         */
 push:
   1:
-	ldc  (0x80009000-0x20+0x00)  -9f; ldpi; 9: ; 
-	ldc  (0x80001000)  -9f; ldpi; 9: ; 
+	LDPI (PUSH_STAT1)
+	LDPI (PUSH_BUF1)
 	call push1
-	ldc  (0x80009000-0x20+0x04)  -9f; ldpi; 9: ; 
-	ldc  ((0x80001000)  + (0x1000) *1)  -9f; ldpi; 9: ; 
+	LDPI (PUSH_STAT2)
+	LDPI (PUSH_BUF2)
 	call push1
 	j 1b
 
-
-
-
-
-
+#define PUSH_STAT_ADDR  (NAUTO+1+1)
+#define PUSH_BUF  (NAUTO+1+0)
+#define NAUTO 2
+#define STATUS 1
+#define UNUSED 0
 push1:
-	ajw - 2 
+	ajw -NAUTO
 
-	 
+	/* Wait until the buffer becomes filled with data */
   1:
 	ldtimer; tin;	// deschedule
-	ldl (2 +1+1) 
+	ldl PUSH_STAT_ADDR
 	ldnl 0
-	stl  1  ; ldl  1  ; 
+	LDUP (STATUS)
 	cj 1b
 
-	 
+	/* Make sure the status word is stable (paranoia) */
   1:
         ldtimer; adc 1; tin;
-	ldl 1 
-	ldl (2 +1+1) 
+	ldl STATUS
+	ldl PUSH_STAT_ADDR
 	ldnl 0
-	stl  1  ; ldl  1  ; 
+	LDUP (STATUS)
 	diff
-	eqc 0; cj  1b
+	BR 1b
 
-	 
-	ldlp 1 	// buffer
-	mint; ldnlp +0x20 	// channel
+	/* Send the status down the output link */
+	ldlp STATUS	// buffer
+	LOAD_PUSH_LINK	// channel
 	ldc 4		// count
 	out
 
-	 
-
-	ldl 1 ;
+	/* If the status is really the end-of-file indicator,
+           there's no need to transmit any data */
+	ldl STATUS;
         eqc -1;
-	eqc 0; cj  1f
+	BR 1f
 
-	   
-	  ldl (2 +1+0) 		// buffer
-	  mint; ldnlp +0x20 	// channel
-	  ldl 1 		// count
+	  /* Send the message proper */
+	  ldl PUSH_BUF		// buffer
+	  LOAD_PUSH_LINK	// channel
+	  ldl STATUS		// count
 	  out
   1:	
-         
+        /* Indicate that the buffer is now empty */
 	ldc 0
-	ldl (2 +1+1) 
+	ldl PUSH_STAT_ADDR
 	stnl 0
 
-	ajw + 2 
+	ajw +NAUTO
 	ret
+#undef PUSH_STAT_ADDR
+#undef PUSH_BUF
+#undef NAUTO
 
 
-
-
-
-	 
-
-
+	/*
+         * Receiver process code
+         */
 pull:
   1:
-	ldc  (0x80009000-0x20+0x08)  -9f; ldpi; 9: ; 
-	ldc  ((0x80001000)  + (0x1000) *2)  -9f; ldpi; 9: ; 
+	LDPI (PULL_STAT1)
+	LDPI (PULL_BUF1)
 	call pull1
-	ldc  (0x80009000-0x20+0x0c)  -9f; ldpi; 9: ; 
-	ldc  ((0x80001000)  + (0x1000) *3)  -9f; ldpi; 9: ; 
+	LDPI (PULL_STAT2)
+	LDPI (PULL_BUF2)
 	call pull1
 	j 1b
 
-
-
-
-
-
+#define PULL_STAT_ADDR  (NAUTO+1+1)
+#define PULL_BUF  (NAUTO+1+0)
+#define NAUTO  2
+#define LENGTH  1
+#define UNUSED  0
 pull1:
-	ajw - 2 
+	ajw -NAUTO
 
-	 
+	/* Wait until the buffer becomes free */
   1:
-	ldl (2 +1+1) 
+	ldl PULL_STAT_ADDR
 	ldnl 0
 	cj 1f
 	ldtimer; tin;	// deschedule
 	j 1b
 
   1:
-	 
-	ldlp 1 	// buffer
-	mint; ldnlp +0x20  	// channel
+	/* Get message length from the input link */
+	ldlp LENGTH	// buffer
+	LOAD_PULL_LINK	// channel
 	ldc 4		// count
 	in
 
-	 
-
-	ldl 1 ;
+	/* If the message length was really the end-of-file indicator,
+           there's no need to receive any data */
+	ldl LENGTH;
         eqc -1;
-	eqc 0; cj  1f
+	BR 1f
 
-	   
-	  ldl (2 +1+0) 		// buffer
-	  mint; ldnlp +0x20  	// channel
-	  ldl 1 		// count
+	  /* Receive the message */
+	  ldl PULL_BUF		// buffer
+	  LOAD_PULL_LINK	// channel
+	  ldl LENGTH		// count
 	  in
   1:
-	 
-	ldl 1 
-	ldl (2 +1+1) 
+	/* Indicate that the buffer is full of data */
+	ldl LENGTH
+	ldl PULL_STAT_ADDR
 	stnl 0
 	
-	ajw + 2 
+	ajw +NAUTO
 	ret
-
-
-
+#undef PULL_STAT_ADDR
+#undef PULL_BUF
+#undef NAUTO
 	
 	
 	
